94359d4046e67ef30ef66158f61d7c9e2a3937ed
Idam Adam <contact@idamadam.com>
2018-08-22 23:22:09 +1000

    Promise chained login and check balance

--- index.js
+++ index.js
@@ -5,21 +5,34 @@ const myki = require('./balance.js');
 const app = express();
 const jsonParser = bodyParser.json();
 
-app.post('/myki/balance', jsonParser, function(req, res) {
-	let auth = req.body
+let debugResponse = {
+	response: "hello"
+}
 
-	var balance = myki.getBalance(auth.username, auth.password)
-			  .then((result) => {
-			  	res.json({'balance': result});
-		      })
-		      .catch((error) => {
-		      	res.sendStatus(401).json({"error": "forbidden"});
-		      });
+app.post('/myki/balance', jsonParser, function(req, res) {
+	let auth = req.body;
+
+	myki.startSession(auth.username, auth.password)
+	.then((result) => {
+		return myki.checkBalance(result.browser, result.page)
+	})
+	.then((result) => {
+		res.json({'balance': result});
+	})
+	.catch((error) => {
+		console.log(error);
+		if (error == "Login Details Incorrect") {
+			res.status(403).json({"error": error})
+		} else {
+			res.status(503).json({"error": error})
+		}
+	});
 })
 
 app.post('/debug', jsonParser, function(req, res) {
+	console.log("Debug Request")
 	console.log(req.body);
-	res.sendStatus(200);
+	res.send(JSON.stringify(debugResponse));
 })
 
 app.listen(3000);
