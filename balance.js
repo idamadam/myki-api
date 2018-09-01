970745b7ea58d665ddfdb2b6e3c4f7217ce816f8
Idam Adam <contact@idamadam.com>
2018-09-01 16:41:27 +1000

    Refactored code for GCloud Functions platform

--- balance.js
+++ balance.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function startSession(username, password) {
-	const browser =  await puppeteer.launch({ headless: true});
+    const browser = await puppeteer.launch({args: ['--no-sandbox']});
 	const page = await browser.newPage();
 
 	await page.goto('https://www.mymyki.com.au/NTSWebPortal/Login.aspx');
--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+'use strict';
+
 const myki = require('./balance.js');
 
-const app = express();
-const jsonParser = bodyParser.json();
+exports.getBalance = async (req, res) => {
+	let auth = req.body
 
-app.post('/myki/balance', jsonParser, function(req, res) {
-	let auth = req.body;
+	if (!auth.username || !auth.password) {
+		return res.send('Please provide username and password');
+	}
 
 	myki.startSession(auth.username, auth.password)
-	.then((result) => {
-		return myki.checkBalance(result.browser, result.page)
-	})
-	.then((result) => {
-		res.json({'balance': result});
-	})
-	.catch((error) => {
-		console.log(error);
-		if (error == "Login Details Incorrect") {
-			res.status(403).json({"error": error})
-		} else {
-			res.status(503).json({"error": error})
-		}
-	});
-})
-
-app.listen(3000);
-
-console.log('Express started on port 3000');
+		.then((result) => {
+			return myki.checkBalance(result.browser, result.page)
+		})
+		.then((result) => {
+			res.json({'balance': result});
+		})
+		.catch((error) => {
+			console.log(error);
+			if (error == "Login Details Incorrect") {
+				res.status(403).json({"error": error})
+			} else {
+				res.status(503).json({"error": error})
+			}
+		});
+}
