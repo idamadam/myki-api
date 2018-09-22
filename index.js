59a00d204217761bdacb72fb9ab212664a558daf
Idam Adam <contact@idamadam.com>
2018-09-22 23:23:47 +1000

    Changed fastbalance function to normal balance function

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 'use strict';
 
 const myki = require('./balance.js');
-const fastMyki = require('./fastBalance');
 
 exports.getBalance = async (req, res) => {
-	let auth = req.body
-
-	if (!auth.username || !auth.password) {
-		return res.status(401).json({"error": "Please provide username and password"});
-	}
-
-	myki.startSession(auth.username, auth.password)
-		.then((result) => {
-			return myki.checkBalance(result.browser, result.page)
-		})
-		.then((result) => {
-			res.json({'balance': result});
-		})
-		.catch((error) => {
-			console.log(error);
-			if (error == "Login Details Incorrect") {
-				res.status(403).json({"error": error})
-			} else {
-				res.status(503).json({"error": error})
-			}
-		});
-}
-
-exports.fastBalance = async (req, res) => {
 	let auth = req.body;
 
 	try {
@@ -36,5 +11,3 @@ exports.fastBalance = async (req, res) => {
 	} catch(e) {
 		res.status(403).json({"error": e.message})
 	}
-
-}
