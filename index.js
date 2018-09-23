605365db925954aed7e58655508a957814aa6cf6
Idam Adam <contact@idamadam.com>
2018-09-23 10:34:12 +1000

    Removed reference to fastBalance

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.getBalance = async (req, res) => {
 	let auth = req.body;
 
 	try {
-		let balance = await fastMyki.checkBalance(auth.username, auth.password);
+		let balance = await myki.checkBalance(auth.username, auth.password);
 		res.json({'balance': balance});
 	} catch(e) {
 		res.status(403).json({"error": e.message})
