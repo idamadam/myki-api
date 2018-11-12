25f953ef75c7f34f52e06ec8464970ed690c75a1
Idam Adam <contact@idamadam.com>
2018-11-12 19:28:21 +1100

    Moved config to it's own files

--- balance.js
+++ balance.js
@@ -1,45 +1,6 @@
 const rp = require('request-promise-native');
 const cheerio = require('cheerio');
-
-let loginForm = {
-    ctl00$uxContentPlaceHolder$browser_name: 'Chrome',
-    ctl00$uxContentPlaceHolder$uxLogin: 'Login'
-}
-
-let balanceForm = {
-    __EVENTTARGET: 'ctl00$uxContentPlaceHolder$uxTimer',
-    _ASYNCPOST: 'true',
-    ctl00$uxHeader$uxSearchTextBox: '',
-    ctl00$uxHeader$hidFontSize: '',
-    ctl00$ScriptManager1: 'ctl00$uxContentPlaceHolder$Panel|ctl00$uxContentPlaceHolder$uxTimer'
-}
-
-let initialRequestOptions = {
-    uri: `https://www.mymyki.com.au/NTSWebPortal/login.aspx`,
-    method: 'GET',
-    simple: false,
-    resolveWithFullResponse: true
-}
-
-let loginOptions = {
-    uri: `https://www.mymyki.com.au/NTSWebPortal/login.aspx`,
-    method: 'POST',
-    simple: false,
-    followAllRedirects: true,
-    resolveWithFullResponse: true,
-    headers: {
-        'Origin': 'https://www.mymyki.com.au',
-        'Referer': 'https://www.mymyki.com.au/NTSWebPortal/login.aspx',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
-    }
-};
-
-let balanceOptions = {
-    uri: `https://www.mymyki.com.au/NTSWebPortal/Registered/MyMykiAccount.aspx?menu=My+myki+account`,
-    method: 'POST',
-    simple: false,
-    followAllRedirects: true
-}
+const config = require('./requestConfig');
 
 function getAspxParams(body) {
     const $ = cheerio.load(body);
@@ -61,6 +22,12 @@ function getAspxParams(body) {
 }
 
 async function checkBalance(username, password) {
+    let loginForm = config.loginForm;
+    let balanceForm = config.balanceForm;
+    let loginOptions = config.loginOptions;
+    let balanceOptions = config.balanceOptions;
+    let initialRequestOptions = config.initialRequestOptions;
+
     const cookieJar = rp.jar();
 
     loginOptions.jar = cookieJar;
